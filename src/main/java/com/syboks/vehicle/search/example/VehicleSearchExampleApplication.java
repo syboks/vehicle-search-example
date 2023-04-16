668e16fa3003136f1c686ec34d3d2cb63eed7215
syboks-coder <58122033+syboks-coder@users.noreply.github.com>
2023-04-17 00:03:46 +0200

    Project initial setup

---
+++ src/main/java/com/syboks/vehicle/search/example/VehicleSearchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.syboks.vehicle.search.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syboks/vehicle/search/example/VehicleSearchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syboks.vehicle.search.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
